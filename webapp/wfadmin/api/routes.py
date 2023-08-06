db6987280043ccef573ba81d9a9cb71d546dfdd5
costagabbie <102681995+costagabbie@users.noreply.github.com>
2023-08-06 16:30:17 -0300

    Changed API login routine to use API

--- webapp/wfadmin/api/routes.py
+++ webapp/wfadmin/api/routes.py
@@ -10,54 +10,22 @@ from base64 import urlsafe_b64decode
 api = Blueprint("api", __name__)
 
 
-@api.route("/api/login")
-def login():
+@api.route("/api/login/<string:api_key>")
+def login(api_key):
     if current_user.is_authenticated:
-        abort(403)
-    request.environ.get("HTTP_X_REAL_IP", request.remote_addr)
-    username = request.values.get("user", "", str)
-    password = request.values.get("pwd", "", str)
-    if (len(username) > 0) and (len(password) > 0):
-        # If the form is being posted and passed the validations
-        # we check if the user and password exist in the database
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.check_password_hash(user.password, password):
-            # the user and password checks out, do the login and redirect
-            login_user(user)
-            return make_response("200", 200)
-        else:
-            # Something went wrong on the username or password
-            try:
-                # So we try to authenticate the user with PAM
-                authenticate(username, password, "login")
-                # But if the user already exist(so the password is now different)
-                if user:
-                    # We update the user
-                    user.password = bcrypt.generate_password_hash(password).decode(
-                        "utf-8"
-                    )
-                    db.session.commit()
-                else:
-                    # The user doesn't exist so we create a new one
-                    user = User(
-                        username=username,
-                        password=bcrypt.generate_password_hash(password).decode(
-                            "utf-8"
-                        ),
-                    )
-                    db.session.add(user)
-                    db.session.commit()
-                # Login and redirect
-                login_user(user)
-                new_login = UserLogin(user_id=user.id, ip_login=request.remote_addr)
-                db.session.add(new_login)
-                db.session.commit()
-                return make_response("200", 200)
-            except PAMError:
-                # If the normal authentication failed, and subsequently the PAM
-                # authentication failed, then we display the failure message
-                return abort(403)
-    return abort(403)
+        return abort(403)
+    user = User.query.filter_by(api_key=api_key)
+    if not user :
+        return abort(403)
+    new_login = UserLogin(
+        user_id = user.id,
+        ip_login = request.remote_addr
+    )
+    db.session.add(new_login)
+    db.session.commit()
+    login_user(user)
+    return make_response('200',200)
+
 
 
 @api.route("/api/endpoint/<string:pubkey>")
